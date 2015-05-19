4878c4fdd51547b614bda25778374c20f240bff9
ryurik <6314166@gmail.com>
2015-05-19 15:39:53 +0300

    EmptyProject

---
+++ CP2Task2/CP2Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP2Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
